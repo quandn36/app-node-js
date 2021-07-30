4ef8ddaaf850d5e8a4deddee440b72f985b91858
quandn36 <quandn36@gmail.com>
2021-07-30 14:31:01 +0700

    learning get post mothod in form submit

--- src/index.js
+++ src/index.js
@@ -8,23 +8,22 @@ const port          = 3000
 
 /**
  ******* SET FOLDER PUBLIC - STATIC FILE *********************
- * 
+ * use là tạo ra 1 cái rule nhưng không phải  là route
 */
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 /**
  ******* HTTP LOGGER DEBUG *********************
  * 
 */
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 /**
  ******* TEMPLATE ENGINE *********************
  * 
 */
-app.engine('hbs', handlebars({ 
-    extname: ".hbs"
-})); // config extension name
+app.engine('hbs', handlebars({ extname: ".hbs" })); // config extension name
 app.set('view engine', 'hbs'); // view engine use handlebars template
 app.set('views', path.join(__dirname, 'resources/views')); // config view engine
 
@@ -32,8 +31,25 @@ app.set('views', path.join(__dirname, 'resources/views')); // config view engine
  ******* ROUTER *********************
  * 
 */
-app.get('/', (req, res) => res.render('home') ); // route home page
-app.get('/news', (req, res) => res.render('news') ); // route news page
+app.get('/', (req, res) => {
+    res.render('home'); // route home page
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');  // route news page
+});
+
+app.get('/search', (req, res) => {
+    res.render('search'); // render view search page
+});
+
+app.post('/search', (req, res) => {
+    
+    console.log(req.body.name);
+    
+    res.send('');
+
+});
 
 
 app.listen(port, () => {
